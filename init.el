482e116ffdac4697a4ee3f229e2b5f09ff0765bc
Dario Oddenino <branch13@gmail.com>
2021-10-13 17:04:19 +0200

    stuff

--- init.el
+++ init.el
@@ -162,7 +162,7 @@
        (php               ; perl's insecure younger brother
         +lsp)
        ;;plantuml          ; diagrams for confusing people more
-       (purescript +lsp)         ; javascript, but functional
+       (purescript)         ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
@@ -170,8 +170,8 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-;;      (rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-;;	 +lsp)
+       (rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+      	 +lsp)
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
